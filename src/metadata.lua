b095599c9b2c45b7f318c61adbe17781d4d9ad92
Bazalbuilder <88984377+Bazalbuilder@users.noreply.github.com>
2024-11-27 21:41:17 -0600

    Update and rename moderation.lua to metadata.lua
    Signed-off-by: Bazalbuilder <88984377+Bazalbuilder@users.noreply.github.com>

---
+++ src/metadata.lua
@@ -0,0 +1,10 @@
+--!strict
+local Metadata = {
+	m_Name = "Aegis Security",
+	m_Creator = "Bazalbuilder",
+	m_License = "MIT",
+	m_Version = "0.1,0",
+	m_Description = "Anti-cheat and exploit mitigation in Roblox",
+}
+
+return Metadata
